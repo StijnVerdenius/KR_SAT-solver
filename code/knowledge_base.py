b933c0b6ca88835936eb166c1c46ede08e0b944c
Maximilian Filtenborg <max.filtenborg@nextpax.com>
2019-02-15 15:42:10 +0100

    Implement set literal strategy

--- code/knowledge_base.py
+++ code/knowledge_base.py
@@ -1,8 +1,12 @@
 from collections import defaultdict
-from typing import List
+from typing import List, Dict, Set
 from code.clause import Clause
 
 class KnowledgeBase:
+
+    bookkeeping: Dict[int, Set[int]]
+    clauses: Dict[int, Clause]
+    current_truth_values: Dict[int, bool]
 
     def __init__(self, clauses = None, current_truth_values = None, bookeeping = None, counter = 0, literal_set = None):
 
@@ -21,7 +25,7 @@ class KnowledgeBase:
         # bookkeeping references
         if bookeeping is None:
             self.bookkeeping = defaultdict(set)
-            for clause in clauses:
+            for clause in clauses.values():
                 for literal in clause.literals:
                     self.bookkeeping[literal].add(clause.id)
 
@@ -66,11 +70,46 @@ class KnowledgeBase:
     def tautology_simplify(self):
         # Currently O(N^2) check
         clauses_to_remove = []
-        for clause in self.clauses:
+        for clause in self.clauses.values():
             if any(-literal in clause.literals for literal in clause.literals):
                 clauses_to_remove.append(clause)
 
         self.remove_clauses(clauses_to_remove)
+
+    def set_literal(self, literal: int, truth_value: bool) -> bool:
+        """
+        Set a literal and its boolean value
+        :param literal:
+        :param truth_value:
+        """
+        # First check if this is a allowed op.
+        for clause_id in self.bookkeeping[literal]:
+            clause = self.clauses[clause_id]
+            if len(clause.literals) == 1 and -literal in clause.literals:
+                return False
+
+        # Set literal
+        self.current_truth_values[literal] = truth_value
+        clauses_to_remove = []
+        for clause_id in self.bookkeeping[literal]:
+            clause = self.clauses[clause_id]
+
+            # If we set P to true and ~P is set, we can remove the literal
+            if -literal in clause.literals:
+                clauses_to_remove.append(clause)
+                continue
+
+            clause.literals.remove(literal)
+
+            # Remove empty and satisfied clauses
+            if len(clause.literals) == 0:
+                clauses_to_remove.append(clause)
+
+        self.remove_clauses(clauses_to_remove)
+        self.bookkeeping[literal].pop()
+
+        return True
+
 
     def remove_clauses(self, clauses_to_remove: List[Clause]):
         """
