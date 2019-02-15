ef0277b22a409a100ca9d97d673ff253b15312e4
Maximilian Filtenborg <max.filtenborg@nextpax.com>
2019-02-15 12:01:10 +0100

    Add some comments

--- code/main.py
+++ code/main.py
@@ -1,1 +1,13 @@
 print("yo mamma")
+
+
+"""
+    1. SAT Solver
+        a. Datastructures
+        b. 
+    2. Read rules of sudoku (dimacs format)
+    3. Read a specific sudoku
+    4. solve that bitch
+    5. Visualize solved sudoku
+    6. Output to dimacs file
+"""
