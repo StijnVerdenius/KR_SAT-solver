f26392524ab7f27766c40881295d1a20baab7cd3
Stijn Verdenius <stijnverdenius@pop-os.localdomain>
2019-02-25 20:05:00 +0100

    reformatting main

--- main.py
+++ main.py
@@ -14,37 +14,50 @@ from solver.stats import print_stats, show_stats
 from solver.visualizer import print_sudoku
 from solver.restart_exception import RestartException
 
-"""
-    1. SAT Solver
-        a. Datastructures
-        b. 
-    2. Read rules of sudoku (dimacs format)
-    3. Read a specific sudoku
-    4. solve that bitch
-    5. Visualize solved sudoku
-    6. Output to dimacs file
-"""
+
+#### Constants
+MIN_ARGUMENTS = 3
+MAX_ARGUMENTS = 3
+MAX_VERSION = 3
+MIN_VERSION = 1
+MIN_PYTHON = 3
+MIN_PYTHON_SUB = 5
 
 
 def main(program_version: int, rules_dimacs_file_path: str):
+    """ main function for solving dimacs """
 
+    settings = get_settings(program_version)
 
     # sudoku_rules_clauses, last_id = read_rules(os.getcwd() + "/../data/sudoku-rules.txt", id=0)
     all_clauses, last_id = read_rules(rules_dimacs_file_path, id=0)
 
-    # all_clauses = list(sudoku_clauses) + list(sudoku_clauses)
     knowledge_base = KnowledgeBase(all_clauses, clause_counter=last_id)
 
     solver = Solver(knowledge_base)
 
-    solution, solved = solver.solve_instance()
+    solution, solved, stats = solver.solve_instance()
 
-    print_sudoku(solution)
+    dimacs = to_dimacs_str(solution)
+
+    file = open(rules_dimacs_file_path+".out", "w")
+
+    file.write(dimacs+"\n")
+
+    file.close()
+
+    print("Solved and written to file sucessfully")
 
 
 def get_settings(program_version: int):
+    """
+    Get settings for run
 
-    if (program_version > 3 or program_version < 1):
+    :param program_version:
+    :return:
+    """
+
+    if (program_version > MAX_VERSION or program_version < MIN_VERSION):
         raise Exception("Program version should be between 1-3")
 
     if program_version == 3:
@@ -80,8 +93,8 @@ def develop(program_version: int, rules_dimacs_file_path: str, problem_path: str
             sudokus_stats = map(solve_fn, problems)
             sudokus_stats = list(filter(lambda x: x[0] is not None, sudokus_stats))
 
-        dm = DataManager(os.getcwd() + '/results/')
-        dm.save_python_obj(sudokus_stats, f"experiment-v{program_version}")
+        # dm = DataManager(os.getcwd() + '/results/')
+        # dm.save_python_obj(sudokus_stats, f"experiment-v{program_version}")
 
     if profile:
         pr.disable()
@@ -105,12 +118,12 @@ def solve_sudoku(problem_id, problem_path, program_version, rules_dimacs_file_pa
             print(f"\nStarting solving problem {problem_id}, program_version: {program_version}")
             print("Loading problem...")
             rules_clauses, last_id = read_rules(rules_dimacs_file_path, id=0)
-            rules_puzzle, is_there_another_puzzle, last_id = read_problem(problem_path, problem_id, last_id)
+            # rules_puzzle, is_there_another_puzzle, last_id = read_problem(problem_path, problem_id, last_id)
 
-            all_clauses = {**rules_clauses, **rules_puzzle}
+            # all_clauses = {**rules_clauses, **rules_puzzle}
 
             # all_clauses = list(sudoku_clauses) + list(sudoku_clauses)
-            knowledge_base = KnowledgeBase(all_clauses, clause_counter=last_id)
+            knowledge_base = KnowledgeBase(rules_clauses, clause_counter=last_id)
             print("Problem loaded")
 
             if settings["Lookahead"]:
@@ -141,26 +154,49 @@ def solve_sudoku(problem_id, problem_path, program_version, rules_dimacs_file_pa
                 runtime = e.runtime
 
 def enforce_python_version():
+    """
+    Enforces correct python version for run
+
+    :return:
+    """
+
     inf = sys.version_info
-    if (inf[0] < 3 or inf[1] < 5):
+    if (inf[0] < MIN_PYTHON or inf[1] < MIN_PYTHON_SUB):
         raise Exception("\n\n####################\nMake sure correct version of python is installed (3.5 or higher)\n####################\n\n")
+
+
+def parse_arguments(arguments):
+    """
+    Parses argument
+
+    :param arguments:
+    :return:
+    """
+    number_of_arguments = len(arguments)
+    if (number_of_arguments < MIN_VERSION or number_of_arguments > MAX_ARGUMENTS):
+        raise Exception(f"You gave {number_of_arguments} arguments while it should be between {MIN_ARGUMENTS} - {MAX_ARGUMENTS}")
+
+    option = arguments[1]
+    if len(option) != 3 or option[0:2] != '-S':
+        raise RuntimeError("Invalid program option")
+
+    program_version = int(option[2])
+
+    input_file = arguments[2]
+
+    return program_version, input_file
 
 if __name__ == "__main__":
 
     enforce_python_version()
 
+    program_version, input_file = parse_arguments(sys.argv)
+
     # Default vars:
-    program_version = 1
+    program_version = 1 # TODO: REMOVE DEFAULT
+    # input_file = os.getcwd() + "/data/sudokus/uf20-01.cnf"
     input_file = os.getcwd() + "/data/sudoku-rules.txt"
 
-    if len(sys.argv) == 3:
-        option = sys.argv[1]
-        if len(option) != 3 or option[0:1] != '-S':
-            raise RuntimeError("Invalid program option")
-        program_version = int(option[2])
-
-        input_file = sys.argv[2]
-
     # main(program_version, input_file)
-    develop(3, input_file, os.getcwd() + "/data/sudokus/1000sudokus.txt")
+    develop(program_version, input_file, os.getcwd() + "/data/sudokus/1000sudokus.txt")
 
